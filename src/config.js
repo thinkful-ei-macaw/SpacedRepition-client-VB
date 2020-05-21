d75a4b1d49bd54ce2211fc04c8176f074e980b96
Brannen Petit <bpetit940@gmail.com>
2020-05-21 11:49:25 -0500

    working on integration

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
   API_ENDPOINT: "https://spaced-repetition-api-vb.herokuapp.com/api",
-  TOKEN_KEY: process.env.REACT_APP_API_KEY ||"blogful-client-auth-token",
+  TOKEN_KEY: process.env.REACT_APP_API_KEY || "blogful-client-auth-token",
 };
