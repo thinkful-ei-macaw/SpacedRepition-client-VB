5f977ff57703beba2e87d7178c5bc2c0e4c91c95
Vendy Prum <vendy@thinkful.com>
2020-05-22 07:44:04 -0700

    input fields

--- src/components/LearningPage/LearningPage.js
+++ src/components/LearningPage/LearningPage.js
@@ -1,8 +1,8 @@
 import React from "react";
-import { Label, Input, Required, Textarea } from "../Form/Form";
+import { Label, Input, Required } from "../Form/Form";
 import Config from "../../config";
 import TokenService from "../../services/token-service";
-import Button from "../Button/Button";
+
 export default class LearningPage extends React.Component {
   constructor(props) {
     super(props);
@@ -17,6 +17,7 @@ export default class LearningPage extends React.Component {
     answer: "",
     isCorrect: false,
     btnClicked: false,
+    userInput: "",
   };
 
   componentDidMount() {
@@ -47,7 +48,7 @@ export default class LearningPage extends React.Component {
 
   submitGuess = (e) => {
     e.preventDefault();
-    let value = this.userInput.current.value;
+    let value = this.state.userInput;
     console.log("this is the value of user input:", value);
     fetch(`${Config.API_ENDPOINT}/language/guess`, {
       method: "POST",
@@ -74,16 +75,25 @@ export default class LearningPage extends React.Component {
           isCorrect: word.isCorrect,
           answer: word.answer,
           btnClicked: true,
+          userInput: "", ///to clear the field after submit
         });
       })
       .catch((err) => console.log(err));
   };
 
-  results() {
-    if (this.state.isCorrect) {
-      return `That's right! Good Job`;
-    }
-  }
+  nextWord = (e) => {
+    e.preventDefault();
+    this.setState({
+      currentWord: this.state.nextWord,
+      userInput: "",
+    });
+  };
+
+  inputChanged = (e) => {
+    this.setState({
+      userInput: e.target.value,
+    });
+  };
 
   //ref.current.value to get from input
   render() {
@@ -96,25 +106,38 @@ export default class LearningPage extends React.Component {
 
     return (
       <div>
-        <p>Total score: {this.state.totalScore}</p>
-        <p>Word correct count: {this.state.wordCorrectCount}</p>
-        <p>Word incorrect count: {this.state.wordIncorrectCount}</p>
-        <p>{this.state.currentWord}</p>
-        <p>{label}</p>
-        <p>
-          <Input type="text" ref={this.userInput} />
-          {requiredLabel}
-        </p>
-        <p>
-          {this.state.btnClicked ? (
-            <button type="submit">Next Word</button>
-          ) : (
-            <button type="submit" onClick={this.submitGuess}>
-              Submit Guess
-            </button>
-          )}
-          {<button type="submit">Next Word</button> && <div>this.state.answer</div>}
-        </p>
+        <form onSubmit={this.submitGuess}>
+          <fieldset>
+            <p>Total score: {this.state.totalScore}</p>
+            <p>Word correct count: {this.state.wordCorrectCount}</p>
+            <p>Word incorrect count: {this.state.wordIncorrectCount}</p>
+            <p>{this.state.currentWord}</p>
+            <p>{label}</p>
+            <p>
+              <Input
+                type="text"
+                value={this.state.userInput}
+                onChange={this.inputChanged}
+              />
+              {requiredLabel}
+            </p>
+
+            {this.state.btnClicked ? (
+              <button onClick={this.nextWord}>Next Word</button>
+            ) : (
+              <button type="submit">Submit Guess</button>
+            )}
+            {<button onClick={this.nextWord}>Next Word</button> && (
+              <div>
+                {this.state.isCorrect ? (
+                  <div>{this.state.answer && " you are correct"}</div>
+                ) : (
+                  this.state.answer
+                )}
+              </div>
+            )}
+          </fieldset>
+        </form>
       </div>
     );
   }
